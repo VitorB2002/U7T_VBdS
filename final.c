4347590499ad47614a1fd4a92ec3dbccf81db2da
VitorB2002 <vitorb.santos2002@gmail.com>
2025-02-21 20:38:43 -0300

    feat: adicionado código básico para uso do display oled

--- final.c
+++ final.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdio.h>
 #include "hardware/adc.h"
 #include "pico/stdlib.h"
 #include <ctype.h>
@@ -115,7 +116,46 @@ int main() {
     uint16_t valor_x, valor_y;
     setup();
 
-    printf("Joystick\n");
+    // Inicialização do i2c
+    i2c_init(i2c1, ssd1306_i2c_clock * 1000);
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C_SDA);
+    gpio_pull_up(I2C_SCL);
+
+    // Processo de inicialização completo do OLED SSD1306
+    ssd1306_init();
+
+    // Preparar área de renderização para o display (ssd1306_width pixels por ssd1306_n_pages páginas)
+    struct render_area frame_area = {
+        start_column : 0,
+        end_column : ssd1306_width - 1,
+        start_page : 0,
+        end_page : ssd1306_n_pages - 1
+    };
+
+    calculate_render_area_buffer_length(&frame_area);
+
+    // zera o display inteiro
+    uint8_t ssd[ssd1306_buffer_length];
+    memset(ssd, 0, ssd1306_buffer_length);
+    render_on_display(ssd, &frame_area);
+
+    restart:
+
+    char *text[] = {
+        "Contador",
+        "Base HEX",
+        "Valor"
+    };
+
+    int y = 0;
+    for (uint i = 0; i < count_of(text); i++)
+    {
+        ssd1306_draw_string(ssd, 5, y, text[i]);
+        y += 8;
+    }
+    render_on_display(ssd, &frame_area);
 
     while (1) {
         joystick_read_axis(&valor_x, &valor_y);
@@ -167,6 +207,29 @@ int main() {
                 } else {
                     printf("X: %d, Y: %d, Botao: %d, Contador: %d\n", valor_x, valor_y, botao_atual, contador);
                 }
+
+                calculate_render_area_buffer_length(&frame_area);
+                memset(ssd, 0, ssd1306_buffer_length);
+                render_on_display(ssd, &frame_area);
+
+                char str[20];
+
+                // Converting integer to string using sprintf
+                sprintf(str, "%d", contador);
+
+                char *update[] = {
+                    "Contador",
+                    "Valor",
+                    str,
+                };
+            
+                int y = 0;
+                for (uint i = 0; i < count_of(update); i++)
+                {
+                    ssd1306_draw_string(ssd, 5, y, update[i]);
+                    y += 8;
+                }
+                render_on_display(ssd, &frame_area);            
             }
 
             gpio_put(BLUE_LED, 1); // Mantém o azul ligado no modo contagem
